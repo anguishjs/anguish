5880bd7f3e91ba512d39e5d4b82c2a5117fde507
rini <rini@rinici.de>
2024-04-16 01:55:57 -0300

    x-if, x-for and mountable components

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
-import { effect, effects, nextTick, reactive } from "./reactivity";
+import { Effect, effect as _effect, enqueue, nextTick, reactive } from "./reactivity";
+
+let effect = _effect;
 
 export const mount = (root: Element = document.body) => {
-  walk(root, reactive({ $refs: {}, $nextTick: nextTick }));
-
-  return () => {
-    effects.forEach(eff => eff.h = true);
-    effects.length = 0;
-  };
+  return context(root, reactive({ $refs: {}, $nextTick: nextTick }));
 };
 
 const kebabToCamel = (str: string) => str.replace(/-./, c => c[1].toUpperCase());
@@ -27,6 +24,12 @@ const specialDirectives: Record<string, (expr: string, scope: any, el: any, arg?
     listen(el, "input", () => scope[expr] = el[attr]);
     // @ts-ignore ???
     effect(() => el[attr] = scope[expr]);
+  },
+  name(expr, scope, el: HTMLTemplateElement) {
+    el.remove();
+    (window as any)[expr] = (props = {}) =>
+      // @ts-ignore yes it does exist
+      component(el.content.cloneNode(true).firstElementChild, props, scope);
   },
 };
 
@@ -66,10 +69,10 @@ const directives: Record<string, (get: () => any, el: any, arg?: string) => void
   html(get, el: HTMLElement) {
     effect(() => el.innerHTML = get());
   },
-  effect: effect,
+  effect: (get) => effect(get),
   init: nextTick,
   show(get, el: HTMLElement) {
-    effect(() => el.style.display = get() ? "none" : "");
+    effect(() => el.style.display = get() ? "" : "none");
   },
   cloak() {},
 };
@@ -77,14 +80,82 @@ const directives: Record<string, (get: () => any, el: any, arg?: string) => void
 const compile = (expr: string, scope: any, el: Element): () => any =>
   Function("$data", "$el", `with($data)return ${expr}`).bind(null, scope, el);
 
-const walk = (el: Element, scope: any) => {
-  const data = el.getAttribute("x-data");
+const component = (el: Element, props: any, scope: any) => {
+  scope = reactive(props, scope);
+  scope.$refs = Object.create(scope.$refs);
+  scope.$root = el;
+  scope.$unmount = context(el, scope);
+  return scope;
+};
 
-  if (data != null) {
-    el.removeAttribute("x-data");
-    scope = reactive(compile(data, scope, el)(), scope);
-    scope.$refs = Object.create(scope.$refs);
-    scope.$root = el;
+const context = (el: Element, scope: any) => {
+  const effects = new Set<Effect>();
+  const eff = effect;
+  effect = (fn) => {
+    eff(fn);
+    effects.add(fn.e!);
+  };
+  walk(el, scope);
+  effect = eff;
+
+  return () => {
+    effects.forEach(eff => (eff.h = true, enqueue(eff)));
+    el.remove();
+  };
+};
+
+const walk = (el: Element, scope: any) => {
+  let expr: string | null;
+  const directive = (attr: string) => {
+    expr = el.getAttribute(attr);
+    el.removeAttribute(attr);
+    return expr != null;
+  };
+
+  if (directive("x-if")) {
+    const get = compile(expr!, scope, el);
+    const anchor = new Text();
+    let clone: Element;
+    let cleanup: () => void;
+    el.replaceWith(anchor);
+    effect(() => {
+      if (get()) {
+        anchor.after(clone = el.cloneNode(true) as Element);
+        cleanup = context(clone, scope);
+      } else {
+        cleanup?.();
+      }
+    });
+
+    return;
+  }
+
+  if (directive("x-for")) {
+    const [terms, arr] = expr!.split(" in ");
+    const [term, idx] = terms.split(",");
+    const get = compile(arr, scope, el);
+    const anchor = new Text();
+    const cleanups = <(() => void)[]> [];
+    el.replaceWith(anchor);
+    effect(() => {
+      const value = [...get()];
+      const diff = cleanups.length - value.length;
+      if (diff < 0) {
+        value.slice(diff).forEach((v, i) => {
+          const clone = el.cloneNode(true) as Element;
+          anchor.before(clone);
+          cleanups.push(context(clone, reactive({ [term]: v, ...idx && { [idx]: i } }, scope)));
+        });
+      } else if (diff > 0) {
+        cleanups.splice(value.length).forEach(f => f());
+      }
+    });
+    return;
+  }
+
+  if (directive("x-data")) {
+    component(el, compile(expr!, scope, el)(), scope);
+    return;
   }
 
   for (const attr of [...el.attributes]) {
--- src/reactivity.ts
+++ src/reactivity.ts
@@ -1,4 +1,5 @@
-export type Effect = { (): void; d: Set<Set<Effect>>; h?: boolean };
+export type Effect = { (): void; d: Set<Set<Effect>>; h?: boolean; };
+type Callback = { (): void; e?: Effect };
 
 const classOf = (obj: any) => obj?.constructor;
 const proxy = (
@@ -31,7 +32,7 @@ const write = (deps: Set<Effect>, target: any, key: keyof any, value: any, recv:
 };
 
 const queue = new Set<Effect>();
-const enqueue = (eff: Effect) => {
+export const enqueue = (eff: Effect) => {
   if (!queue.size) {
     nextTick(() => {
       queue.forEach(eff => eff());
@@ -57,8 +58,7 @@ export const reactive = (scope: any, parent: any = {}) => {
 };
 
 let running: Effect;
-export const effects = <Effect[]> [];
-export const effect = (fn: () => void) => {
+export const effect = (fn: Callback) => {
   const execute: Effect = () => {
     execute.d.forEach(d => d.delete(execute));
     execute.d.clear();
@@ -73,6 +73,5 @@ export const effect = (fn: () => void) => {
   };
 
   execute.d = new Set();
-  effects.push(execute);
-  enqueue(execute);
+  enqueue(fn.e = execute);
 };
